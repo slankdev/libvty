2c78746e32e4f8659023c1da24abec94397ea85e
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-09 17:15:45 +0900

    2017.07.09-17:15

--- susanow/vty_cmd_history.h
+++ susanow/vty_cmd_history.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <string>
 
+
 class commandhistory {
   size_t hist_index;
   std::vector<std::string> history;
@@ -18,5 +19,17 @@ class commandhistory {
   std::string shallow_get();
 };
 
+inline std::string commandhistory::deep_get()
+{
+  if (history.empty()) return "";
+  if (hist_index+2 > history.size()) return *(history.end() - hist_index - 1);
+  return *(history.end() - ++hist_index);
+}
+inline std::string commandhistory::shallow_get()
+{
+  if (history.empty()) return "";
+  if (ssize_t(hist_index)-1 < 0) return *(history.end() - hist_index - 1);
+  return *(history.end() - --hist_index - 1);
+}
 
 
