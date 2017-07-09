d98ba3fd718246ab2a347b513f3b4f04399eb59b
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-07-09 15:40:38 +0900

    2017.07.09-15:40

--- susanow/main.cc
+++ susanow/main.cc
@@ -1,22 +1,24 @@
 
 
+#include <stdint.h>
 #include <thread>
 #include <ssn_vty.h>
 
+void Slankdev(void*) {}
+
 int main(int argc, char** argv)
 {
-  ssn_vty vty(0x00000000, 9999);
+  auto addr = 0x00000000;
+  auto port = 9999;
+  ssn_vty vty(addr, port);
+  vty.new_install_command("", Slankdev, nullptr);
 
+  printf("Listen %u:%u\n", addr, port);
   std::thread t(ssn_vty_poll_thread, &vty);
   getchar();
   ssn_vty_poll_thread_stop();
   t.join();
 }
 
-void Slankdev(void*) {}
 
-int new_main(int argc, char** argv)
-{
-  ssn_vty vty(0x0, 9999);
-  vty.new_install_command("", Slankdev, nullptr);
-}
+
